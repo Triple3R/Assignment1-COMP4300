dec4df4c727a8fcc784eec9936182fe088724947
R.Razoelizafy <razoriana@proton.me>
2025-12-25 15:33:19 +0300

    Simple start. Configured CMake, tested ImGui and SFML. Finished writing assignment goals in README

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,37 @@
+#include <memory>
+#include <fstream>
+#include <vector>
 #include <SFML/Graphics.hpp>
+#include <imgui.h>
+#include <imgui-SFML.h>
 
 int main()
 {
-    auto window = sf::RenderWindow(sf::VideoMode({1920u, 1080u}), "CMake SFML Project");
-    window.setFramerateLimit(144);
+    sf::RenderWindow window(sf::VideoMode({ 200, 200 }), "SFML works!");
+    ImGui::SFML::Init(window);  // ImGui-SFML
+
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
+
+    sf::Clock deltaClock;  // ImGui delta time
 
     while (window.isOpen())
     {
         while (const std::optional event = window.pollEvent())
         {
+            ImGui::SFML::ProcessEvent(window, *event);  // ImGui Events
+
             if (event->is<sf::Event::Closed>())
-            {
                 window.close();
-            }
         }
 
+        ImGui::SFML::Update(window, deltaClock.restart());  // Update ImGui
+
         window.clear();
+        window.draw(shape);
+        ImGui::SFML::Render(window);  // Render ImGui
         window.display();
     }
+
+    ImGui::SFML::Shutdown();  // Cleanup
 }
